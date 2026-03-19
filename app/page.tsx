737a72c3e40abd3a7b6ed244013d31e6cf49eefc
GregFullStack <gregoire@alyas.fr>
2026-03-19 21:36:48 +0100

    bite

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,6 @@ export default async function Home() {
   return (
     <>
       <HeroSection />
-      <Integrations />
-      <PricingTable subscriptionDetails={subscriptionDetails} />
-      <FooterSection />
     </>
   );
 }
